26ad81298b5bf31358e8a382fee459f9932b6ba3
nag <sailunag18@gmail.com>
2022-03-18 16:50:51 +0530

    Enable Eureka Registration of service

--- src/main/java/com/shailendra/itemInventory/Application.java
+++ src/main/java/com/shailendra/itemInventory/Application.java
@@ -3,6 +3,7 @@ package com.shailendra.itemInventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.shailendra.itemInventory.*")
 @EnableJpaRepositories("com.shailendra.itemInventory.*")
 @EntityScan("com.shailendra.itemInventory.*")
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
